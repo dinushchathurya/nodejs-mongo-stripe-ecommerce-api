8da95b5c336bf6fb9d818b1c2905d7e10a71f0f0
dinushchathurya <dinushchathurya21@gmail.com>
2021-10-01 13:51:19 +0530

    refactor: Hashing password with bcrypt

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,19 +1,29 @@
 const User = require('../models/User');
+const bcrypt = require('bcryptjs');
 
 const AuthController = {
 
     async create_user(req, res, next) {
+
         const newUser = new User({
             username: req.body.username,
             email: req.body.email,
-            password: req.body.password
+            password: bcrypt.hashSync(req.body.password, 10)
         });
 
         try {
             const user = await newUser.save();
-            res.status(201).json(user)
+            res.status(201).json({
+                type : 'success',
+                message: "User has been added successfuly",
+                user
+            })
         } catch (err) {
-            res.status(500).json(err)
+            res.status(500).json({
+                type: "error",
+                message: "Something went wrong please try again",
+                err
+            })
         }
     },
 };
