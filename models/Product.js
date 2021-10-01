efd11e356dcec4414e071d50bbb0ad02927787b0
dinushchathurya <dinushchathurya21@gmail.com>
2021-10-01 11:41:02 +0530

    feat: Create all required model schemas

---
+++ models/Product.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const ProductSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    categories: {
+        type: Array,
+    },
+    size: {
+        type: String
+    },
+    color: {
+        type: String
+    },
+    price: {
+        type: Number,
+        required: true
+    }
+},
+    { timestamps: true }
+);
+
+module.exports = mongoose.model('Product', ProductSchema);
