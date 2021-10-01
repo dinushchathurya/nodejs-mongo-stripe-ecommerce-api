2487cdb49711ef98c6b2da97c8fb5a0f3585ce8a
dinushchathurya <dinushchathurya21@gmail.com>
2021-10-01 21:33:35 +0530

    feat: Create controller function to get single products

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -19,6 +19,30 @@ const ProductController = {
         }
     },
 
+    /* get single product */
+    async get_product(req, res) {
+        try {
+            const product = await Product.findById(req.params.id);
+            if(!product) {
+                res.status(200).json({
+                    type: "error",
+                    message: "Product doesn't exists"
+                })
+            } else{
+                res.status(200).json({
+                    type: "success",
+                    product
+                })
+            }   
+        } catch (err) {
+            res.status(500).json({
+                type: "error",
+                message: "Something went wrong please try again",
+                err
+            })
+        }
+    },
+
     /* create new product */
     async create_product(req, res) {
         const newProduct = new Product(req.body);
--- routes/product.js
+++ routes/product.js
@@ -5,6 +5,7 @@ const { ProductController } = require('../controllers');
 const { isAdminVerifier } = require('../middlewares/verifyToken');
 
 router.get('/', isAdminVerifier, ProductController.get_products);
+router.get('/:id', isAdminVerifier, ProductController.get_product);
 router.post('/', isAdminVerifier, ProductController.create_product);
 
 module.exports = router;
