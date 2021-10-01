5113ad906089a619dc8c4c8a1f8c8df03812437a
dinushchathurya <dinushchathurya21@gmail.com>
2021-10-01 19:47:28 +0530

    feat: Create fi=unction to get user stats

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -2,16 +2,84 @@ const User = require('../models/User');
 
 const UserController = {
 
-    async update_user(req, res, next) {
+    /* get all users */
+    async get_users(req, res) {
+        try {
+            const users = await User.find();
+            res.status(200).json({
+                type: "success",
+                users
+            });
+        } catch (err) {
+            res.status(500).json({
+                type: "error",
+                message: "Something went wrong please try again",
+                err
+            })
+        }
+       
+    },
+
+    /* get single user */
+    async get_user(req, res) {
+        try {
+            const user = await User.findById(req.params.id);
+            const { password, ...data } = user._doc;
+            res.status(200).json({
+                type: "success",
+                data
+            });
+
+        } catch (err) {
+            res.status(500).json({
+                type: "error",
+                message: "Something went wrong please try again",
+                err
+            })
+        }
+    },
+
+    /* get user stats */
+    async get_stats(req, res) {
+        const date = new Date();
+        const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
+        try {
+            const data = await User.aggregate([
+                { $match : { 
+                    createdAt: { $gte: lastYear }
+                }},
+                { $project: { 
+                    month: { $month: "$createdAt"}
+                }},
+                { $group : {
+                    _id: "$month", 
+                    total: { $sum: 1},
+                }}
+            ]);
+            res.status(200).json({
+                type: "success",
+                data
+            })
+        } catch (err) {
+            res.status(500).json({
+                type: "error",
+                message: "Something went wrong please try again",
+                err
+            })
+        }
+    },
+
+    /* update user */
+    async update_user(req, res) {
         if(req.body.password) {
             req.body.password = bcrypt.hashSync(req.body.password, 10)
         }
         
         try {
             const updatedUser = await User.findByIdAndUpdate(req.params.id, {
-                    $set: req.body
-                }, 
-                { new: true }
+                $set: req.body
+            }, 
+            { new: true }
             );
             res.status(200).json({
                 type: "success",
@@ -26,6 +94,23 @@ const UserController = {
             })
         }
     },
+
+    /* delete user */
+    async delete_user(req, res) {
+        try {
+            await User.findByIdAndDelete(req.params.id)
+            res.status(200).json({
+                type: "success",
+                message: "User has been deleted successfully"
+            });
+        } catch (err) {
+            res.status(500).json({
+                type: "error",
+                message: "Something went wrong please try again",
+                err
+            })
+        }
+    }
 };
 
 module.exports = UserController;
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-const { accessLevelVerifier } = require('../middlewares/verifyToken');
+const { accessLevelVerifier, isAdminVerifier } = require('../middlewares/verifyToken');
 const { UserController } = require('../controllers');
 
+router.get('/', isAdminVerifier, UserController.get_users);
+router.get('/:id', isAdminVerifier, UserController.get_user);
+router.get('/stats', isAdminVerifier, UserController.get_stats);
 router.put('/:id', accessLevelVerifier, UserController.update_user);
+router.delete('/:id', isAdminVerifier, UserController.delete_user);
 
 module.exports = router;
