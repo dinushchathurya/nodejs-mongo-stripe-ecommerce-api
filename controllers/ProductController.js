41424f81d1705d3f7a964ee8c3c960b3f7e5f8a6
dinushchathurya <dinushchathurya21@gmail.com>
2021-10-01 21:23:08 +0530

    feat: Create controller function to get all products

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -2,6 +2,23 @@ const Product = require('../models/Product');
 
 const ProductController = {
     
+    /* get all products */
+    async get_products(req, res) {
+        try {
+            const products = await Product.find();
+            res.status(200).json({
+                type: "success",
+                products
+            })
+        } catch (err) {
+            res.status(500).json({
+                type: "error",
+                message: "Something went wrong please try again",
+                err
+            })
+        }
+    },
+
     /* create new product */
     async create_product(req, res) {
         const newProduct = new Product(req.body);
--- routes/product.js
+++ routes/product.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const { ProductController } = require('../controllers');
 const { isAdminVerifier } = require('../middlewares/verifyToken');
 
+router.get('/', isAdminVerifier, ProductController.get_products);
 router.post('/', isAdminVerifier, ProductController.create_product);
 
 module.exports = router;
