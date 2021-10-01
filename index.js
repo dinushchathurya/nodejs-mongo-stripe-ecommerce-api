17ba8792c44dfdcbac7b62515a1651c8248f59a1
dinushchathurya <dinushchathurya21@gmail.com>
2021-10-01 09:07:40 +0530

    config: Connect Databse with the application

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+const dbConfig = require('./config/database-config');
+
+// Connecting to the database
+mongoose.connect(dbConfig.URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => {
+    console.log("Successfully connected to the database");
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...', err);
+    process.exit();
+});
+
+// listen for requests
+app.listen(3000, () => {
+    console.log("Server is listening on port 3000");
+});
